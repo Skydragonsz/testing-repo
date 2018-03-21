19e9cee22a479b8c86f38ebad23f53bcdbe4ab85
SkyDragonsz <Arthur_saprunov@hotmail.com>
2018-03-21 13:54:55 +0100

    Testing bots (#1,#2,#3)

---
+++ javafile.java
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+<<<<<<< HEAD
+// First todo didn't trigger todobot
+//^ what happens when I remove this todo?
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+
+>>>>>>> ff5846c... Testing todobot
+
+
+//@todo Testing todobot with body
+//@body This is the body, extra info should go here
+<<<<<<< HEAD
+=======
+>>>>>>> fd4087b... Testing todobot
+=======
+>>>>>>> a71e975... Emptied java file
+=======
+//@todo Testing todobot with body
+//@body This is the body, extra info should go here
+>>>>>>> 5771ba1... Added new todo in java file (this should trigger a issue)
+=======
+
+
+/**
+ * @todo Testing todobot with body (the other one doesn't trigger)
+ * @body This is the body, extra info should go here
+ * does a second line work?
+ */
+>>>>>>> ec69db6... Adding a todo
